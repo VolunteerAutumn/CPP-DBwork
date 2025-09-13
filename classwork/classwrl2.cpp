680f0eeb72e07b4a82ac69851ac8d79d66f32c62
VolunteerAutumn <draqoshaimposter@gmail.com>
2025-09-13 11:52:21 +0300

    Create classwrl2.cpp

---
+++ classwork/classwrl2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/// PART 1
+// TASK I
+int main()
+{
+	cout << "Name       : \"The war and the peace\"" << endl;
+	cout << "Avotor     : L.N. Tolstoi" << endl;
+	cout << "Izdatelstvo: Piter" << endl;
+	cout << "Pages      : 500" << endl;
+}
+
+
